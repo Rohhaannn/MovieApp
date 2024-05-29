218c147cec73877172d788cad3fb7859c4d0bf84
Rohan <shingaderohan96@gmail.com>
2024-05-29 22:32:21 +0530

    Routing defining (on-going)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom' 
+import Home from './components/Home/Home';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import MovieDetails from './components/MovieDetails/MovieDetails';
+
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='app'>
+        <Router>
+          <Header/>
+          <Route path='/' Component={Home}/>
+          <Route path='/movie/:imdbID' Component={MovieDetails}/>
+          <Route Component={PageNotFound}/>
+          <Footer/>
+        </Router>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      // video till : 17:09
     </>
   )
 }
