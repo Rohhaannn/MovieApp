a977033abf1524332a0b8bf3ca91eb1f28d23a6d
Rohan <shingaderohan96@gmail.com>
2024-05-30 11:54:37 +0530

    Router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,18 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom' 
-import Home from './components/Home/Home';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import PageNotFound from './components/PageNotFound/PageNotFound';
-import MovieDetails from './components/MovieDetails/MovieDetails';
+// import React, { useState } from 'react'
 
-import './App.scss'
+// import './App.scss'
+// import Layout from './components/Layout';
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   // const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div className='app'>
-        <Router>
-          <Header/>
-          <Route path='/' Component={Home}/>
-          <Route path='/movie/:imdbID' Component={MovieDetails}/>
-          <Route Component={PageNotFound}/>
-          <Footer/>
-        </Router>
-      </div>
-      // video till : 17:09
-    </>
-  )
-}
 
-export default App
+
+//   return (
+//     <>
+
+//     </>
+//   )
+// }
+
+// export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom' 
+import Home from './components/Home/Home';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import MovieDetails from './components/MovieDetails/MovieDetails';
+import Layout from './Layout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/movie/:imdbID' element={<MovieDetails/>}/>
+      <Route path='*' element={<PageNotFound/>}/>
+    </Route>
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
