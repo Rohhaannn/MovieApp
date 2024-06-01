ede4d2f7e95ae35e9b76a3fdd7e307f866b27fe0
Rohan <shingaderohan96@gmail.com>
2024-06-01 20:38:15 +0530

    basic container styling added

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import './App.scss'
 
 const Layout = () => {
   return (
     <>
       <Header/>
-      <Outlet/>
+      <div className='container'>
+        <Outlet/>
+      </div>
       <Footer/>
     </>
   )
